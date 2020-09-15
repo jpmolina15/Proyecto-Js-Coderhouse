37c2aba316970c5b439c6c582b88255cfe9bfa8c
jpmolina15 <jpm15@MacBook-Pro.local>
2020-09-15 13:03:11 -0300

    Update index.js
    Agregado precios

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const camisetaPrecio = 5600;
 const camperaPrecio = 4500;
 const zapasPrecio = 5000;
 
+let precio = null
+
+const precioTotal = (producto1, producto2, producto3) => {
+    precio = producto1 + producto3 + producto2
+}
+
 const productos = [{
     nombre: "Camiseta de Boca",
     precio: camisetaPrecio
