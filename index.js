02407d28ba612689e2a6f3fae84a41868c63082b
jpmolina15 <jpm15@MacBook-Pro.local>
2020-09-15 12:54:21 -0300

    Agregado js1

---
+++ index.js
@@ -0,0 +1,15 @@
+const camisetaPrecio = 5600;
+const camperaPrecio = 4500;
+const zapasPrecio = 5000;
+
+const productos = [{
+    nombre: "Camiseta de Boca",
+    precio: camisetaPrecio
+}, {
+    nombre: "Campera de Boca",
+    precio: camperaPrecio
+},
+{
+    nombre: "Zapatillas",
+    precio: zapasPrecio
+}]
