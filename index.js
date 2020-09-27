0f71acfba7edd026950fa70addc1e97807fc3d2b
jpmolina15 <jpm15@MacBook-Pro.local>
2020-09-27 20:13:22 -0300

    localStorage pre tabla

--- index.js
+++ index.js
@@ -7,41 +7,53 @@ let precio = null
 const precioTotal = (producto1, producto2, producto3) => {
     precio = producto1 + producto3 + producto2
 }
-var usuario = prompt('Cual es tu nombre de usuario?')
+
 
 let productosSeccion = document.querySelector('#productos')
 let filtrosSeccion = document.querySelector('#filtros')
 let carritoSeccion = document.querySelector('#carrito')
 
 let tituloProd = carritoSeccion.querySelector('h2')
-tituloProd.textContent = 'Carrito de ' + usuario
+tituloProd.textContent = 'Carrito de '
 
 
 
-// 
-// const productos = [{
-//     nombre: "Camiseta de Boca",
-//     precio: camisetaPrecio,
-//     marca: "Adidas",
-//     rubro: "Fútbol",
-//     tipo: "Camiseta"
-// }, {
-//     nombre: "Campera de Boca",
-//     precio: camperaPrecio,
-//     marca: "Adidas",
-//     rubro: "Casual",
-//     tipo: "Campera"
-// },
-// {
-//     nombre: "Zapatillas de Correr",
-//     precio: zapasPrecio,
-//     marca: "Adidas",
-//     rubro: "Running",
-//     tipo: "Zapatillas"
-// }]
 
-// let carrito = ["Camiseta de Boca", "Campera de Boca"]
-// carrito.push("zapatillas")
-// let carritoAnterior = ["botines", "pelota"]
 
-// let nuevoCarrito = carrito.concat(carritoAnterior)
+const productos = [{
+    id: 1,
+    nombre: "Camiseta de Boca",
+    precio: camisetaPrecio,
+    marca: "Adidas",
+    rubro: "Fútbol",
+    tipo: "Camiseta",
+    cantidad: 0
+}, {
+    id: 2,
+    nombre: "Campera de Boca",
+    precio: camperaPrecio,
+    marca: "Adidas",
+    rubro: "Casual",
+    tipo: "Campera",
+    cantidad: 0
+},
+{   
+    id: 3,
+    nombre: "Zapatillas de Correr",
+    precio: zapasPrecio,
+    marca: "Adidas",
+    rubro: "Running",
+    tipo: "Zapatillas",
+    cantidad: 0
+}]
+
+
+const agregar = () => {
+    for (let i = 0; i < productos.length; i++){
+        let id = productos[i].id
+            productos[i].cantidad = document.querySelector('#casaca' + id).value
+             localStorage.setItem('producto' + i, JSON.stringify(productos[i]))
+    }
+}
+agregar()
+
