0b65e4f8767fab5280f5beea18ec18c860bcfcbc
jpmolina15 <jpm15@MacBook-Pro.local>
2020-09-27 21:49:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,6 @@ let zapasPrecio = 5000;
 
 let precio = null
 
-const precioTotal = (producto1, producto2, producto3) => {
-    precio = producto1 + producto3 + producto2
-}
-
-
-let productosSeccion = document.querySelector('#productos')
-let filtrosSeccion = document.querySelector('#filtros')
-let carritoSeccion = document.querySelector('#carrito')
-
-let tituloProd = carritoSeccion.querySelector('h2')
-tituloProd.textContent = 'Carrito de '
-
-
-
-
 
 const productos = [{
     id: 1,
@@ -47,33 +32,25 @@ const productos = [{
     cantidad: 0
 }]
 
+let precios = []
 
-const agregar = () => {
-    for (let i = 0; i < productos.length; i++) {
-        let id = productos[i].id
-        productos[i].cantidad = document.querySelector('#casaca' + id).value
-        localStorage.setItem('producto' + i, JSON.stringify(productos[i]))
-    }
-}
-agregar()
-
-const carro = () => {
-    // Refe Tabla
+const agregarCarrito = () => {
     var table = document.querySelector("#tablaCarrito");
-    let precioFinal = []
-    // Crea la tabla
-    for (var i = 0; i < productos.length; i++) {
-        
-        
-        // Producto
+    
+    for (let i = 0; i < productos.length; i++) {
+        // Info del producto
         const index = productos[i].id;
         const nombre = productos[i].nombre;
-        const cantidad = productos[i].cantidad;
+        const cantidad = document.querySelector('#casaca' + index).value;
         const precio = productos[i].precio * cantidad
-        
-        precioFinal.push(precio)
-        
-        // Crea las hileras de los productos
+
+        //  Agrega a los precios
+        precios.push(precio)
+
+        // Local Storage de productos elegidos
+        localStorage.setItem('producto' + i, JSON.stringify(productos[i]))
+
+        // Filas de productos en carritos
         if (cantidad >= 1) {
             var hilera = document.createElement("tr");
 
@@ -98,14 +75,15 @@ const carro = () => {
         }
     }
 
-    // Precio Final
-    let total = precioFinal.reduce((a, b) => a + b, 0);
+    // Precio Final Calculo
+    let total = precios.reduce((a, b) => a + b, 0);
     const cuenta = document.querySelector('#final')
     const monto = document.createTextNode('$' + total)
 
     cuenta.appendChild(monto)
-     
-    
+
+
 
 }
-carro()
+
+agregarCarrito()
