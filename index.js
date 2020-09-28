cbf5e950a80804a86dcbfc3fcb6a9d47349ee2ce
jpmolina15 <jpm15@MacBook-Pro.local>
2020-09-28 13:13:47 -0300

    Todo Joya

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let camisetaPrecio = 5600;
 let camperaPrecio = 4500;
 let zapasPrecio = 5000;
 
-let precioFinal = []
+
 
 const productos = [{
     id: 1,
@@ -14,47 +14,57 @@ const productos = [{
     cantidad: 0
 }, {
     id: 2,
-    nombre: "Campera de Boca",
-    precio: camperaPrecio,
-    marca: "Adidas",
-    rubro: "Casual",
-    tipo: "Campera",
-    cantidad: 0
-},
-{
-    id: 3,
     nombre: "Zapatillas de Correr",
     precio: zapasPrecio,
     marca: "Adidas",
     rubro: "Running",
     tipo: "Zapatillas",
     cantidad: 0
+},
+{
+    id: 3,
+    nombre: "Campera de Boca",
+    precio: camperaPrecio,
+    marca: "Adidas",
+    rubro: "Casual",
+    tipo: "Campera",
+    cantidad: 0
+
 }]
 
 
-
 const agregarCarrito = () => {
-    var table = document.querySelector("#tablaCarrito");
-
+    const table = document.querySelector("#tablaCarrito");
+    let precioFinal = []
     for (let i = 0; i < productos.length; i++) {
         // Info del producto
         const index = productos[i].id;
         const nombre = productos[i].nombre;
-        const cantidad = document.querySelector('#casaca' + index).value;
+        const cantidad = document.querySelector('#producto' + index).value;
         const precio = productos[i].precio * cantidad
-
         //  Agrega a los precios
         precioFinal.push(precio)
 
         // Local Storage de productos elegidos
         localStorage.setItem('producto' + i, JSON.stringify(productos[i]))
 
-        // Filas de productos en carritos
+
+
+        // Limpia carrito
+        if (cantidad >= 0) {
+            if (document.querySelector('.hilera' + index)) {
+                const fila = document.querySelector('.hilera' + index)
+                fila.parentNode.removeChild(fila)
+            }
+        }
+
+        //Agrega carrito
         if (cantidad >= 1) {
             var hilera = document.createElement("tr");
+            hilera.classList.add('hilera' + index)
 
-            for (var j = 0; j < 3; j++) {
-                //Columnas
+            for (var j = 0; j < productos.length; j++) {
+                // Hileras
                 var celda = document.createElement("td");
                 var textoCelda = document.createTextNode("cel " + i + ", col" + j);
 
@@ -73,9 +83,15 @@ const agregarCarrito = () => {
             table.appendChild(hilera);
         }
     }
+
     // Precio Final Calculo
     let total = precioFinal.reduce((a, b) => a + b, 0);
     document.querySelector('#final').textContent = '$' + total
 }
+for (let i = 0; i < productos.length; i++) {
+    // Info del producto
+    const index = productos[i].id;
+    document.querySelector("#btn" + index).onclick = agregarCarrito
+}
 
-agregarCarrito()
+
