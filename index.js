1a128e87c45708cb9d89c3b9f76addfdc40cc761
jpmolina15 <jpm15@MacBook-Pro.local>
2020-09-27 21:25:34 -0300

    Falta añadir eventos

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const productos = [{
     tipo: "Campera",
     cantidad: 0
 },
-{   
+{
     id: 3,
     nombre: "Zapatillas de Correr",
     precio: zapasPrecio,
@@ -49,11 +49,63 @@ const productos = [{
 
 
 const agregar = () => {
-    for (let i = 0; i < productos.length; i++){
+    for (let i = 0; i < productos.length; i++) {
         let id = productos[i].id
-            productos[i].cantidad = document.querySelector('#casaca' + id).value
-             localStorage.setItem('producto' + i, JSON.stringify(productos[i]))
+        productos[i].cantidad = document.querySelector('#casaca' + id).value
+        localStorage.setItem('producto' + i, JSON.stringify(productos[i]))
     }
 }
 agregar()
 
+const carro = () => {
+    // Refe Tabla
+    var table = document.querySelector("#tablaCarrito");
+    let precioFinal = []
+    // Crea la tabla
+    for (var i = 0; i < productos.length; i++) {
+        
+        
+        // Producto
+        const index = productos[i].id;
+        const nombre = productos[i].nombre;
+        const cantidad = productos[i].cantidad;
+        const precio = productos[i].precio * cantidad
+        
+        precioFinal.push(precio)
+        
+        // Crea las hileras de los productos
+        if (cantidad >= 1) {
+            var hilera = document.createElement("tr");
+
+            for (var j = 0; j < 3; j++) {
+                //Columnas
+                var celda = document.createElement("td");
+                var textoCelda = document.createTextNode("cel " + i + ", col" + j);
+
+                if (j === 0) {
+                    textoCelda = document.createTextNode(nombre);
+                }
+                if (j === 1) {
+                    textoCelda = document.createTextNode(cantidad);
+                }
+                if (j === 2) {
+                    textoCelda = document.createTextNode('$' + precio);
+                }
+                celda.appendChild(textoCelda);
+                hilera.appendChild(celda);
+            }
+            table.appendChild(hilera);
+        }
+    }
+
+    // Precio Final
+    let total = precioFinal.reduce((a, b) => a + b, 0);
+    const cuenta = document.querySelector('#final')
+    const monto = document.createTextNode('$' + total)
+
+    cuenta.appendChild(monto)
+     
+    
+
+}
+carro()
